e146f9d1437205097474b5fad338791fb05babcb
hamzachikar <chikar.h@outlook.fr>
2020-10-22 13:06:46 +0100

    adding the stories nav

--- src/app/home/home-page/storie-nav/storie-nav.component.ts
+++ src/app/home/home-page/storie-nav/storie-nav.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'storie-nav',
   templateUrl: './storie-nav.component.html',
   styleUrls: ['./storie-nav.component.scss']
 })
-export class StorieNavComponent implements OnInit {
-
+export class StorieNavComponent implements OnInit,AfterViewInit {
+  @ViewChild('stories') private scroller: ElementRef;
+  scrollDiv=0;
+  storieScroll=0;
   constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.scrollDiv=this.scroller.nativeElement.scrollWidth/90;
   }
 
+  ngOnInit(): void {
+
+  }
+  onScroll(direct:string){
+
+    if(direct==="left"){
+      console.log((this.scroller.nativeElement.scrollWidth-this.scroller.nativeElement.scrollLeft)/210)
+      if(this.storieScroll<this.scrollDiv){
+
+        if((this.scroller.nativeElement.scrollWidth-this.scroller.nativeElement.scrollLeft)/210>1){
+          this.storieScroll=this.storieScroll+3
+          this.scroller.nativeElement.scrollLeft =  this.scroller.nativeElement.scrollLeft+210;
+        }
+      }
+    }
+    else{
+      if(this.storieScroll>0){
+        this.storieScroll=this.storieScroll-3;
+        this.scroller.nativeElement.scrollLeft =  this.scroller.nativeElement.scrollLeft-210;
+      }
+      else{
+        this.storieScroll=0
+      }
+    }
+   // console.log(this.storieScroll);
+  }
 }
